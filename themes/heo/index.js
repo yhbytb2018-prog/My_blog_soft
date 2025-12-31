1e126932ba88ba86563f489fa7779ad4d1343f35
yhbytb2018-prog <yhbytb2018@gmail.com>
2026-01-01 07:59:54 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -396,7 +396,7 @@ const Layout404 = props => {
                 <h1 className='error-title font-extrabold md:text-9xl text-7xl dark:text-white'>
                   404
                 </h1>
-                <div className='dark:text-white'>请尝试站内搜索寻找文章</div>
+                <div className='dark:text-white'>请尝试站内搜索寻找其它文章</div>
                 <SmartLink href='/'>
                   <button className='bg-blue-500 py-2 px-4 text-white shadow rounded-lg hover:bg-blue-600 hover:shadow-md duration-200 transition-all'>
                     回到主页
@@ -407,7 +407,7 @@ const Layout404 = props => {
 
             {/* 404页面底部显示最新文章 */}
             <div className='mt-12'>
-              <LatestPostsGroup {...props} />
+              {<LatestPostsGroup {...props} />}
             </div>
           </Transition>
         </div>
