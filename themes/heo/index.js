8fdd07a84e4dc850bd20bf5749cdead0c2443d42
yhbytb2018-prog <yhbytb2018@gmail.com>
2026-01-01 08:19:20 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -388,7 +388,7 @@ const Layout404 = props => {
               <LazyImage
                 className='error-img h-60 md:h-full p-4'
                 src={
-                  'https://bu.dusays.com/2023/03/03/6401a7906aa4a.gif'
+                  'https://pibed.qqsea.top/file/page/image/ovMgCjT8.jpeg'
                 }></LazyImage>
 
               {/* 右侧文字 */}
